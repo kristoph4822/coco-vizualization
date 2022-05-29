fd41490dfbfebc37851a2d84676b6b77a113c268
Krzysztof Maciejewski <kristoph4822@gmail.com>
2022-05-29 15:04:05 +0200

    changed setup for requirements

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 from app import app
+import webbrowser
+from threading import Timer
 
 PORT = 5000
 
 if __name__ == '__main__':
+    Timer(1, lambda: webbrowser.open(f"http://127.0.0.1:{PORT}")).start()
     app.run(port=PORT)
     
